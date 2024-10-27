0dfb3bcca6e238f7610e917791af7ff8122426c5
akash <akashs1132000@gmail.com>
2024-10-27 20:44:32 +0530

    Summing Even Numbers ombining filter + reduce:

--- sumOfEven.js
+++ sumOfEven.js
@@ -3,7 +3,16 @@
 // Filter out the odd numbers and then sum only the even numbers in an array.
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for loop
+let output = 0;
+for(let i = 0; i <= numbers.length -1 ; i++){
+    if (numbers[i] % 2 == 0){
+        output += numbers[i];
+    }
+}
+console.log(output);
 
+//Filter and reduce
 const evenSum = numbers.filter(number => number % 2 === 0)
     .reduce((acc, curr) => acc += curr, 0);
 
